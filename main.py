1b966b317c2971ed802eac4fe2f752df8e2095f0
rprtr258 <poletansky@gmail.com>
2021-08-09 22:24:19 +0300

    f

---
+++ main.py
@@ -0,0 +1,33 @@
+import urllib
+import subprocess
+
+from flask import Flask, request
+app = Flask(__name__)
+ 
+@app.route("/")
+def latex():
+    if request.args.get('f'):
+        formula = request.args.get('f').replace(' ', '+')
+        print(repr(formula))
+        res = "https://latex.codecogs.com/gif.latex?%5Cdpi%7B300%7D%20%5Chuge%20" + urllib.request.pathname2url(formula.replace(' ', "&space;")).replace('/', '%5C')
+        print(repr(res))
+        return res
+    elif request.args.get('p'):
+        pyth_prog = request.args.get('p').replace(' ', '+').replace('_', ' ')
+        print(repr(pyth_prog))
+        with open("c", "w") as fd:
+            fd.write(pyth_prog)
+        ps = subprocess.run(['echo', '42'], check=True, capture_output=True)
+        processNames = subprocess.run(["python", "pyth/pyth.py", "c"], input=ps.stdout, capture_output=True)
+        out = processNames.stdout.decode('utf-8')
+        out = out.replace('\r', '').replace('\n', ' ')
+        if "/timeout" in out:
+            return "пососи"
+        if "/ban" in out:
+            return "охуел?"
+        print(repr(out))
+        return out
+ 
+if __name__ == "__main__":
+    print(repr(urllib.request.pathname2url("\\".replace(' ', "&space;"))))
+    app.run()
