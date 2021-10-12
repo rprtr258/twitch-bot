c310427554ce059392458dba1a810d80174df3c9
rprtr258 <poletansky@gmail.com>
2021-10-12 05:21:47 +0300

    Autocommit

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ with open("model.json", "r") as fd:
     vocabulary = load(fd)["prior"].keys()
 
 
-@app.route("/")
-def index():
-    if request.args.get('p'):
-        pyth_prog = request.args.get('p').replace(' ', '+').replace('_', ' ')
-        print(repr(pyth_prog))
-        with open("/app/c", "w") as fd:
-            fd.write(pyth_prog)
-        ps = run(['echo', '42'], check=True, capture_output=True)
-        processNames = run(["python", "pyth/pyth.py", "c"], input=ps.stdout, capture_output=True)
-        out = processNames.stdout.decode('utf-8')
-        out = out.replace('\r', '').replace('\n', ' ')
-        for w in ["ban", "disconnect", "timeout", "unban", "slow", "slowoff", "followers", "followersoff", "subscribers", "subscribersoff", "clear", "uniquechat", "uniquechatoff", "emoteonly", "emoteonlyoff", "commercial", "host", "unhost", "raid", "unraid", "marker"]:
-            if f"/{w}" in out or f".{w}" in out:
-                return "пососи"
-        print(repr(out))
-        return out
+@app.route("/p")
+def pyth():
+    if not request.args.get('p'):
+        return "provide program with 'p' request parameter"
+    pyth_prog = request.args.get('p').replace(' ', '+').replace('_', ' ')
+    print(repr(pyth_prog))
+    with open("/app/c", "w") as fd:
+        fd.write(pyth_prog)
+    ps = run(['echo', '42'], check=True, capture_output=True)
+    processNames = run(["python", "pyth/pyth.py", "c"], input=ps.stdout, capture_output=True)
+    out = processNames.stdout.decode('utf-8')
+    out = out.replace('\r', '').replace('\n', ' ')
+    for w in ["ban", "disconnect", "timeout", "unban", "slow", "slowoff", "followers", "followersoff", "subscribers", "subscribersoff", "clear", "uniquechat", "uniquechatoff", "emoteonly", "emoteonlyoff", "commercial", "host", "unhost", "raid", "unraid", "marker"]:
+        if f"/{w}" in out or f".{w}" in out:
+            return "пососи"
+    print(repr(out))
+    return out
 
 cache = {}
 def levenshteinDistance(s1, s2):
