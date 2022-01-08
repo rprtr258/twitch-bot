6510eb70dd80ef41868defbe610e24d6188e07e9
rprtr258 <poletansky@gmail.com>
2022-01-08 16:58:22 +0300

    Remove temporary file for pyth

--- src/commands/pyth/__init__.py
+++ src/commands/pyth/__init__.py
@@ -5,16 +5,19 @@ import commands.utils
 import config
 import utils
 
+PYTH_CMD_FILE = "c"
+
 
 @commands.utils.if_empty_message("Provide pyth program: pyth.readthedocs.io")
 def pyth(conf: config.Config, message_record: utils.MessageRecord):
     if message_record.username == "passionsausage":
         return "PassionSausage наказан за абьюз системы AAUGH"
-    with open("c", "w", encoding="utf-8") as fd:
+    with open(PYTH_CMD_FILE, "w", encoding="utf-8") as fd:
         fd.write(message_record.message)
     ps = subprocess.run(['echo', '6'], check=True, capture_output=True)
     # TODO: stop on timeout (5-10 seconds)
-    processNames = subprocess.run(["python3", "pyth/pyth.py", "c"], input=ps.stdout, capture_output=True)
+    processNames = subprocess.run(["python3", "pyth/pyth.py", PYTH_CMD_FILE], input=ps.stdout, capture_output=True)
+    subprocess.run(["rm", PYTH_CMD_FILE])
     out = processNames.stdout.decode('utf-8').replace('\r', '').replace('\n', ' ')
     logging.info("PYTH PROGRAM: %s OUTPUT: %s", repr(message_record.message), repr(out))
     if any(cmd in out for cmd in conf.pyth_config.ban_words):
