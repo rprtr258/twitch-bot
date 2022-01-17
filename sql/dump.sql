a84dee902d62f3c61ac6040827a6c018af435cfd
rprtr258 <poletansky@gmail.com>
2022-01-17 20:54:27 +0300

    Add tasks description, add db dumping

--- sql/dump.sql
+++ sql/dump.sql
@@ -1,3 +1,6 @@
-select * from balaboba;
-select * from emote;
-select * from feed;
+.mode insert balaboba
+.out blab.sql
+SELECT * FROM balaboba;
+.mode insert feed
+.out feed.sql
+SELECT * FROM feed WHERE count > 0;
