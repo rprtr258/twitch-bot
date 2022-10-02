47814e0e3019ff62c939a947880e77c6b4d12899
Poletansky Viktor <poletansky@gmail.com>
2022-10-02 20:11:45 +0300

    add deploy config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,4 +3,17 @@ module.exports = {
     name: "twitch-bot",
     script: "just run",
   }],
+  deploy : {
+    production : {
+      user: "root",
+      host: "176.126.113.161",
+      key: "/home/rprtr258/.ssh/test_vds",
+      ref: "origin/master",
+      repo: "https://github.com/rprtr258/twitch-bot-api.git",
+      path: "/var/www/twitch-bot",
+      // TODO: deploy just executable
+      // "pre-setup": "go install github.com/rprtr258/rwenv && apt install just", // and install go
+      "post-deploy": "go mod download"
+    }
+  }
 };
