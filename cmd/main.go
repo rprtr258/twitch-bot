35e7342fffa9fe5f02c20e757088d38bba57ebdd
Poletansky Viktor <poletansky@gmail.com>
2022-11-01 16:38:48 +0300

    fix import

--- cmd/main.go
+++ cmd/main.go
@@ -22,9 +22,9 @@ import (
 
 	// TODO: rename packages
 	"github.com/rprtr258/twitch-bot/internal"
-	_ "github.com/rprtr258/twitch-bot/internal/migrations"
 	"github.com/rprtr258/twitch-bot/internal/permissions"
 	"github.com/rprtr258/twitch-bot/internal/services"
+	_ "github.com/rprtr258/twitch-bot/migrations"
 )
 
 // TODO: show partially content
